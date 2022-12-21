2cea4cb46c8b6ca9768c8bb1af609aea42cc4e0a
FedericoGedda <115471970+FedericoGedda@users.noreply.github.com>
2022-12-21 14:34:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <cmath>
+#include <stdlib.h>
 using namespace std;
 
+float funz(float y){
+    float x=pow(y,2)*cos(y)+1;
+    return x;
+}
+
 int main() {
-   return 0;
+    float a,b,x=0,err=0;
+
+    do {
+    cout<<"inserire estremi: ";
+    cin>>a>>b;
+    }while ((funz(a)*funz(b)+1)>=0);
+
+    do {
+        a=(a+b)/2;
+        if (funz(x)==0){
+            cout<<x;
+            cout<<funz(x);
+            return 0;
+        } else {
+            if (funz(a)*funz(b)<0){
+                b=x;
+            } else {
+                a=x;
+            }
+            err=abs((b-a)/2);
+        }
+
+    }while (err>=(1e-6));
+    cout << int(x * 10000) / 10000.0 <<endl;
+    return 0;
 }
